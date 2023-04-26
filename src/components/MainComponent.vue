d9d69f41cd4c7411caa9510dd5c76725239b4828
peppeluc22 <gluca0260@gmail.com>
2023-04-26 15:46:17 +0200

    imported sliderComponent and created his arrays

--- src/components/MainComponent.vue
+++ src/components/MainComponent.vue
@@ -15,6 +15,12 @@
         <section class="row myContainer py-5">
             <SpecialComponent />
         </section>
+
+        <!-- First Slider with Our Products -->
+        <section>
+            <SliderComponent :sliderArray="productsList" :slide="productsList.object" :title="titlePro"
+                :topListItem="navItemFirst.object" :topListArray="navItemFirst" />
+        </section>
     </div>
 </template>
 
@@ -23,13 +29,15 @@ import JumbotronComponent from './JumbotronComponent.vue';
 import MainNav from './MainNav.vue';
 import JumboCard from './JumboCard.vue';
 import SpecialComponent from './SpecialComponent.vue';
+import SliderComponent from './SliderComponent.vue';
 export default {
     name: 'MainComponent',
     components: {
         JumbotronComponent,
         MainNav,
         JumboCard,
-        SpecialComponent
+        SpecialComponent,
+        SliderComponent
     },
     data() {
         return {
@@ -56,7 +64,33 @@ export default {
                     text: 'World Of Tanks New Game',
                     img: 'img/cms-banner-04.jpg'
                 }
-            ]
+            ],
+            productsList: [
+                {
+                    img: 'img/01.jpg',
+                    rating: 4,
+                    name: 'Shopping Mahjang connect',
+                    firstPrice: `&pound;180.00`,
+                    secondPrice: `&pound;100.00`
+                }
+            ],
+            dealList: [
+
+            ],
+            navItemFirst: [
+                'Featured',
+                'New Arrival',
+                'Best Sellers'
+            ],
+            navItemSecond: [
+                '1026 days',
+                '13 hours',
+                '24 mins',
+                '17 secs'
+            ],
+            titlePro: 'Our Products',
+            titleDeal: 'Deal Of The Day'
+
         }
     }
 }
