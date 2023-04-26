d1312cf0d343ddce09e4f983844b891c0d7e6c85
peppeluc22 <gluca0260@gmail.com>
2023-04-26 12:23:41 +0200

    created navbar

--- src/components/NavbarComponent.vue
+++ src/components/NavbarComponent.vue
@@ -1,13 +1,74 @@
 <template>
-    <div>
+    <div class="myContainer py-4 d-flex justify-content-between">
+        <!-- Site logo -->
+        <div class="logo-container align-items-center">
+            <img src="img/logo.png" alt="logo" class="w-100 h-100">
+        </div>
 
+        <!-- Navbar items -->
+        <div>
+            <ul class="d-flex h-100 align-items-center">
+                <li v-for="(item, index) in navItems" :key="index" class="px-4">
+                    <a href="#" @click="getActive(index)" :class="index === currentIndex ? 'active' : ''">{{ item }}</a>
+                </li>
+            </ul>
+        </div>
+
+        <!-- User utilities -->
+        <div class="text-white d-flex align-items-center">
+
+            <i class="fa-solid fa-magnifying-glass"></i>
+            <i class="fa-regular fa-user px-4"></i>
+
+            <div class="d-flex align-items-center">
+                <i class="fa-solid fa-bag-shopping px-2"></i>
+                <p class="bag-price m-0">(0) &pound;0.00</p>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
-    name: 'NavbarComponent'
+    name: 'NavbarComponent',
+    data() {
+        return {
+            navItems: [
+                'Home',
+                'Shop',
+                'Blog',
+                'Media',
+                'ShortCode',
+                'Features'
+            ],
+            currentIndex: true
+        }
+    },
+    methods: {
+        getActive(index) {
+            this.currentIndex = index
+        }
+    }
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+@use '../assets/style/partials/variables' as *;
+
+ul {
+    list-style: none;
+
+    a {
+        text-decoration: none;
+        color: white;
+    }
+}
+
+.active {
+    color: $secondary-color;
+}
+
+.bag-price {
+    color: $secondary-color;
+}
+</style>
