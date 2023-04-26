209c8d754cd08b7cb566ee7659aa2eba6c04a887
peppeluc22 <gluca0260@gmail.com>
2023-04-26 11:12:06 +0200

    first commit

---
+++ src/components/NavbarComponent.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'NavbarComponent'
+}
+</script>
+
+<style lang="scss" scoped></style>
---
+++ src/components/TopHeader.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'TopHeader'
+}
+</script>
+
+<style lang="scss" scoped></style>
