8884347cf80bae15d0119526694578dff24d375c
peppeluc22 <gluca0260@gmail.com>
2023-04-26 12:23:50 +0200

    added Navbar

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,19 @@
 <template>
+  <!-- Header with top-header and navbar -->
   <header>
     <TopHeader />
     <hr class="text-white p-0 m-0">
+    <NavbarComponent />
   </header>
 </template>
 
 <script>
 import TopHeader from './components/TopHeader.vue';
+import NavbarComponent from './components/NavbarComponent.vue';
 export default {
   components: {
     TopHeader,
+    NavbarComponent
   }
 }
 </script>
