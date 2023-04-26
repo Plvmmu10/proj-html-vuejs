6514f9e035d363eb34b5ea871a4827560eff7cbf
peppeluc22 <gluca0260@gmail.com>
2023-04-26 11:46:18 +0200

    created topHeader

--- src/components/TopHeader.vue
+++ src/components/TopHeader.vue
@@ -1,6 +1,21 @@
 <template>
-    <div>
+    <div class="myContainer py-2 d-flex justify-content-between">
+        <!-- Div with wishlist and need help -->
+        <div class="d-flex align-items-center">
+            <p class="subtitle pe-4">Whishlist(0)</p>
 
+            <p class="m-0">|</p>
+
+            <p class="subtitle ps-4">
+                Need Help ?
+                <i class="fa-solid fa-chevron-down subtitle"></i>
+            </p>
+        </div>
+
+        <!-- div with phone number -->
+        <div>
+            <p class="subtitle">Call 24/7.800-8754-987</p>
+        </div>
     </div>
 </template>
 
