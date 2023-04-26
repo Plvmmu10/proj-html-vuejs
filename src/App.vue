6e368e57f03912c143f1b7039548c5dcda9fcfc9
peppeluc22 <gluca0260@gmail.com>
2023-04-26 12:48:02 +0200

    added MainComponent

--- src/App.vue
+++ src/App.vue
@@ -5,15 +5,22 @@
     <hr class="text-white p-0 m-0">
     <NavbarComponent />
   </header>
+
+  <!-- Main with jumbo and all the sections -->
+  <main>
+    <MainComponent />
+  </main>
 </template>
 
 <script>
 import TopHeader from './components/TopHeader.vue';
 import NavbarComponent from './components/NavbarComponent.vue';
+import MainComponent from './components/MainComponent.vue';
 export default {
   components: {
     TopHeader,
-    NavbarComponent
+    NavbarComponent,
+    MainComponent
   }
 }
 </script>
