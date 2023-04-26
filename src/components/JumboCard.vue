b90559f8aab58680d27e7bf576c7e0c411f11f5f
peppeluc22 <gluca0260@gmail.com>
2023-04-26 14:13:34 +0200

    created jumboCard

---
+++ src/components/JumboCard.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="j-card col-6">
+        <img :src="item.img" :alt="item.text">
+
+        <div class="j-content">
+            <div class="upcoming">
+                <h3 class="m-0">{{ item.genre }}</h3>
+            </div>
+
+            <h3 class="m-0 text-white">{{ item.text }}</h3>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'JumboCard',
+    props: [
+        'item'
+    ]
+}
+</script>
+
+<style lang="scss" scoped>
+.j-card {
+    position: relative;
+
+    img {
+        width: 100%;
+        display: block;
+        height: 100%;
+    }
+
+    .j-content {
+        position: absolute;
+        top: 35%;
+        left: 55%;
+    }
+}
+</style>
