895afb122239c8cfef4f691def520c63143b6ccb
peppeluc22 <gluca0260@gmail.com>
2023-04-26 12:47:40 +0200

    created Jumbotron

---
+++ src/components/JumbotronComponent.vue
@@ -0,0 +1,52 @@
+<template>
+    <div id="hero" class="d-flex align-items-center">
+        <div class="myContainer">
+            <div class="d-flex flex-column w-25">
+
+                <div id="upcoming">
+                    <h4 class="m-0 p-1 fw-bold">Upcoming Stock</h4>
+                </div>
+
+                <div class="text-white mt-3 mb-5">
+                    <h1>Resident Evil 3 Remake</h1>
+                </div>
+
+                <div>
+                    <button class="btn rounded-0 fw-bold">Shop Now</button>
+                </div>
+
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'JumbotronComponent'
+}
+</script>
+
+<style lang="scss" scoped>
+@use '../assets/style/partials/variables' as *;
+
+#hero {
+    width: 100%;
+    height: 600px;
+    background-image: url('img/parallax.jpg');
+    background-size: cover;
+    background-position: center;
+
+    #upcoming {
+        background-color: $secondary-color;
+        width: 200px;
+    }
+
+    .btn {
+        background-color: $light-color;
+
+        &:hover {
+            background-color: $secondary-color;
+        }
+    }
+}
+</style>
