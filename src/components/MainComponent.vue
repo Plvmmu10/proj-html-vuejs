248df1a040e85bfbfa02f70e9e7e3c59f6cd7958
peppeluc22 <gluca0260@gmail.com>
2023-04-26 12:47:49 +0200

    added Jumbotron

---
+++ src/components/MainComponent.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <JumbotronComponent />
+    </div>
+</template>
+
+<script>
+import JumbotronComponent from './JumbotronComponent.vue';
+export default {
+    name: 'MainComponent',
+    components: {
+        JumbotronComponent
+    }
+}
+</script>
+
+<style lang="scss" scoped></style>
