e194c4abcbf2bea8b38072324cce062f3f27fafa
peppeluc22 <gluca0260@gmail.com>
2023-04-27 11:11:47 +0200

    blogscomponent scaffholding

---
+++ src/components/BlogsComponent.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="myContainer">
+        <div class="d-flex flex-column align-items-center">
+            <h1 class="text-white">New Game Blogs</h1>
+            <div class="horizontal"></div>
+        </div>
+
+        <div class="row">
+            <div class="col-4">
+
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'BlogsComponent',
+    data() {
+        return {
+            // blogsList: [
+            //     {
+            //         img: 'img/At-vero-eos-et-accusamus-1400x878_t.jpg',
+            //         date: 'Jan 18,2021',
+            //         text: 'Anyway REPS is a NYC agency repres enting photographers'
+            //     },
+            //     {
+            //         img: 'img/At-vero-eos-et-accusamus-1400x878_t.jpg',
+            //         date: 'Jan 18,2021',
+            //         text: 'Anyway REPS is a NYC agency repres enting photographers'
+            //     },
+            //     {
+            //         img: 'img/At-vero-eos-et-accusamus-1400x878_t.jpg',
+            //         date: 'Jan 18,2021',
+            //         text: 'Anyway REPS is a NYC agency repres enting photographers'
+            //     },
+            // ]
+        }
+    }
+}
+</script>
+
+<style lang="scss" scoped></style>
