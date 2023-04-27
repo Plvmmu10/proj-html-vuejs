e809ffc519b42960b552754a9dd762b3d7d04228
peppeluc22 <gluca0260@gmail.com>
2023-04-27 13:46:45 +0200

    added footerComponent

--- src/App.vue
+++ src/App.vue
@@ -8,17 +8,27 @@
   <main>
     <MainComponent />
   </main>
+
+  <footer>
+    <FooterComponent />
+  </footer>
 </template>
 
 <script>
 import HeaderComponent from './components/HeaderComponent.vue';
 import MainComponent from './components/MainComponent.vue';
+import FooterComponent from './components/FooterComponent.vue';
 export default {
   components: {
     HeaderComponent,
-    MainComponent
+    MainComponent,
+    FooterComponent
   }
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+footer {
+  background-image: url('/img/footer-bg.png');
+}
+</style>
