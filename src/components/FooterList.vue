9a81581798204db7ec4e3e1faf8205f39024c6dd
peppeluc22 <gluca0260@gmail.com>
2023-04-27 13:47:52 +0200

    created FooterList

---
+++ src/components/FooterList.vue
@@ -0,0 +1,126 @@
+<template>
+    <div class="col-3">
+        <h4>Address Info</h4>
+
+        <ul class="subtitle">
+            <li id="address">
+                <p>
+                    60 29th Street San Francisco, CA 94110 507-Union Trade Center, United States of America
+                </p>
+            </li>
+
+            <li id="number">
+                <p>
+                    (+00) 123-456-789
+                </p>
+            </li>
+
+            <li id="email">
+                <p>
+                    demo@example.com
+                </p>
+            </li>
+        </ul>
+    </div>
+
+    <div class="col-3" v-for="item in footerArray">
+        <h4>{{ item.title }}</h4>
+
+        <ul>
+            <li v-for="listItem in item.liList">
+                <a href="#">{{ listItem }}</a>
+            </li>
+        </ul>
+    </div>
+
+    <!-- Newsletter col -->
+    <div class="col-3">
+        <h4>Our Newsletter</h4>
+
+        <p class="subtitle">
+            There are many variations of passages of form humour or randomised
+        </p>
+
+        <div class="input-group mt-4">
+            <input type="text" name="emailInput" id="emailInput" class="form-control" aria-describedby="basic-addon2">
+
+            <div class="input-group-append">
+                <span class="input-group-text" id="basic-addon2">
+                    d
+                </span>
+            </div>
+        </div>
+
+
+        <div class="social-box d-flex">
+            <p v-for="social in socialArray" class="py-4 px-2"><i :class="social"></i></p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'FooterList',
+    props: ['footerArray', 'socialArray']
+}
+</script>
+
+<style lang="scss" scoped>
+@use '../assets/style/partials/variables' as *;
+
+h4 {
+    color: $light-color;
+}
+
+ul {
+    padding: 0;
+
+    li {
+        display: flex;
+
+        a {
+            text-decoration: none;
+            color: rgba(211, 211, 211, 0.6);
+
+            &:hover {
+                color: $secondary-color;
+            }
+        }
+    }
+
+    #address:before {
+        font-family: 'Font Awesome 6 Free';
+        content: '\f3c5';
+        font-weight: 900;
+        margin: 0 15px 0 0px;
+        color: $secondary-color;
+    }
+
+    #number:before {
+        font-family: 'Font Awesome 6 Free';
+        content: '\f095';
+        font-weight: 900;
+        margin: 0 15px 0 0px;
+        color: $secondary-color;
+    }
+
+    #email:before {
+        font-family: 'Font Awesome 6 Free';
+        content: '\f0e0';
+        font-weight: 900;
+        margin: 0 15px 0 0px;
+        color: $secondary-color;
+    }
+}
+
+.social-box {
+    i {
+        color: $light-color;
+
+        &:hover {
+            cursor: pointer;
+            color: $secondary-color;
+        }
+    }
+}
+</style>
