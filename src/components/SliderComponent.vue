fa68b5d02e48558133db2c729864496b7fd41d32
peppeluc22 <gluca0260@gmail.com>
2023-04-27 19:53:54 +0200

    CREATED A WORKING SLIDER

--- src/components/SliderComponent.vue
+++ src/components/SliderComponent.vue
@@ -7,17 +7,18 @@
             <TopSlider :listItem="topListItem" :listArray="topListArray" />
         </div>
 
-        <div class="row flex-nowrap">
-            <SliderCard v-for="(slide, index) in sliderArray" :key="index" :item="slide" />
-
+        <div class="position-relative">
+            <div class="d-flex flex-nowrap row" ref="sliderContainer">
+                <SliderCard v-for="(slide, index) in sliderArray" :key="index" :item="slide" />
+            </div>
             <!-- Change slide buttons -->
-            <!-- <button class="prevBtn myBtn" @click="prevCard">
+            <button class="prevBtn myBtn" @click="prevCard">
                 <i class="fa-solid fa-chevron-left"></i>
             </button>
 
             <button class="nextBtn myBtn" @click="nextCard">
                 <i class="fa-solid fa-chevron-right"></i>
-            </button> -->
+            </button>
         </div>
     </div>
 </template>
@@ -41,67 +42,30 @@ export default {
     ],
     data() {
         return {
-            // activeSlider: [],
-            currentIndex: 0,
+            sliderContainer: null,
+            sliderWidth: null
         }
     },
     methods: {
-        // addActive(currentIndex) {
-        //     this.activeSlider = [];
+        nextCard() {
+            this.sliderContainer.scrollBy(this.sliderWidth / 4, 0);
 
-        //     let activeIndex = currentIndex;
+            if ((this.sliderContainer.scrollLeft + this.sliderWidth + 5) >= this.sliderContainer.scrollWidth) {
+                this.sliderContainer.scrollLeft = 0
+            }
+        },
+        prevCard() {
+            this.sliderContainer.scrollBy(- this.sliderWidth / 4, 0);
 
-        //     // this.activeSlider.push(this.sliderArray[activeIndex])
-        //     // this.activeSlider.push(this.sliderArray[activeIndex + 1])
-        //     // this.activeSlider.push(this.sliderArray[activeIndex + 2])
-        //     // this.activeSlider.push(this.sliderArray[activeIndex + 3])
-
-        //     // this.activeSlider.push(this.sliderArray[0])
-        //     // this.activeSlider.push(this.sliderArray[1])
-        //     // this.activeSlider.push(this.sliderArray[2])
-        //     // this.activeSlider.push(this.sliderArray[3])
-
-        //     if ((activeIndex + 3) > this.sliderArray.length - 1) {
-        //         this.activeSlider.push(this.sliderArray[0])
-        //         this.activeSlider.push(this.sliderArray[1])
-        //         this.activeSlider.push(this.sliderArray[2])
-        //         this.activeSlider.push(this.sliderArray[3])
-        //     } else {
-        //         this.activeSlider.push(this.sliderArray[activeIndex])
-        //         this.activeSlider.push(this.sliderArray[activeIndex + 1])
-        //         this.activeSlider.push(this.sliderArray[activeIndex + 2])
-        //         this.activeSlider.push(this.sliderArray[activeIndex + 3])
-        //     }
-
-
-        // },
-        // removeActive() {
-        // },
-        // nextCard() {
-
-        //     if (this.currentIndex === this.sliderArray.length - 1) {
-        //         this.currentIndex = 0
-        //     } else {
-        //         this.currentIndex++
-        //     }
-
-        //     // this.addActive(this.currentIndex)
-
-        // },
-        // prevCard() {
-        //     if (this.currentIndex === 0) {
-        //         this.currentIndex = this.sliderArray.length - 1
-        //     } else {
-        //         this.currentIndex--
-        //     }
-
-        //     this.removeActive(this.currentIndex)
-
-        // }               
+            if (this.sliderContainer.scrollLeft === 0) {
+                this.sliderContainer.scrollLeft = this.sliderContainer.scrollWidth
+            }
+        }
     },
-    // mounted() {
-    //     this.addActive(this.currentIndex);
-    // }
+    mounted() {
+        this.sliderContainer = this.$refs.sliderContainer;
+        this.sliderWidth = this.sliderContainer.offsetWidth;
+    }
 
 }
 </script>
@@ -114,47 +78,58 @@ hr {
     color: $secondary-color;
 }
 
+.myBtn {
+    width: 35px;
+    height: 35px;
+    border-radius: 50%;
+    background-color: transparent;
+    border: 1px solid rgb(211, 211, 211, 1);
+    color: rgb(211, 211, 211, 1);
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    transition: .3s;
+
+    i {
+        font-size: 15px;
+    }
+
+    &:hover {
+        color: $secondary-color;
+        background-color: $light-color;
+    }
+}
+
+.prevBtn {
+    position: absolute;
+    left: -2%;
+    bottom: 50%;
+    z-index: 1000;
+    translate: 0% -80%;
+}
+
+.nextBtn {
+    position: absolute;
+    right: -2%;
+    bottom: 50%;
+    translate: 0% -80%;
+}
+
 .row {
-    position: relative;
     overflow-x: auto;
     overflow-y: hidden;
 
-    .myBtn {
-        width: 35px;
-        height: 35px;
-        border-radius: 50%;
-        background-color: transparent;
-        border: 1px solid rgb(211, 211, 211, 1);
-        color: rgb(211, 211, 211, 1);
-        display: flex;
-        align-items: center;
-        justify-content: center;
-        transition: .3s;
-
-        i {
-            font-size: 15px;
-        }
-
-        &:hover {
-            color: $secondary-color;
-            background-color: $light-color;
-        }
+    // Scrollbar config
+    &::-webkit-scrollbar {
+        display: none;
     }
 
-    .prevBtn {
-        position: absolute;
-        left: 0;
-        bottom: 50%;
-        z-index: 1000;
-        translate: 0% -80%;
-    }
+    -ms-overflow-style:none;
 
-    .nextBtn {
-        position: absolute;
-        right: 0;
-        bottom: 50%;
-        translate: 0% -80%;
-    }
+    scrollbar-width:none;
+
+    scroll-behavior: smooth;
+
 }
 
 .active {
